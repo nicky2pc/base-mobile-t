2c82be5a349c53dad7fdc89abb503dd8dfda68d3
nick <nick@nicks-MacBook-Air.local>
2026-01-14 11:33:04 +0500

    release

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-// app.tsx
 import { sdk } from '@farcaster/miniapp-sdk';
 import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    sdk.actions.ready();
+    const init = async () => {
+      const isInMiniApp = await sdk.isInMiniApp();
+      if (isInMiniApp) {
+        sdk.actions.ready();
+      }
+    };
+    init();
   }, []);
 
   return <div>Hello World</div>;
